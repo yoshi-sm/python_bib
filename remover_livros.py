3cfc75d094a3846b68f0c28876475ee1dd17032d
Yoshi <yoshismiranda@gmail.com>
2020-10-18 19:40:02 -0300

    remover livros

---
+++ remover_livros.py
@@ -0,0 +1,29 @@
+from saveload import *
+
+
+# remove todos os livros que vieram antes do ano limite do BD
+def rmv_ano(ano_limite):
+    dicionario = carregar()
+    temp = []
+    for i in range(len(dicionario["ano"])):
+        if dicionario["ano"][i] < ano_limite:
+            temp.append(i)
+
+    for i in reversed(temp):
+        for key in dicionario:
+            dicionario[key].pop(i)
+    salvar(dicionario)
+
+
+
+def rmv_titulo(titulo):
+    dicionario = carregar()
+    temp = []
+    for i in range(len(dicionario["titulo"])):
+        if dicionario["titulo"][i] == titulo:
+            temp.append(i)
+
+    for i in reversed(temp):
+        for key in dicionario:
+            dicionario[key].pop(i)
+    salvar(dicionario)

255cf29ff6ffccae8a0dde7499f20a73dc319dc5
Yoshi <yoshismiranda@gmail.com>
2020-10-19 13:38:36 -0300

    sei la

--- saveload.py
+++ saveload.py
@@ -1,13 +1,13 @@
 import json
 
 #salva o dicionario parametro no arquivo "testando.dat" no formato json
-def salvar(dicionario):
+def salvar_dicio(dicionario):
     with open("testando.dat", "w") as f:
         f.write(json.dumps(dicionario))
 
 
 #carrega o dicionario
-def carregar():
+def carregar_dicio():
     with open("testando.dat") as f:
         return json.loads(f.read())
 

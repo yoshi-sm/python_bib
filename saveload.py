1cdc1d2d91c94e9a1839b5354308547ecb804795
Yoshi <yoshismiranda@gmail.com>
2020-10-19 13:44:51 -0300

    reverter

--- saveload.py
+++ saveload.py
@@ -1,13 +1,13 @@
 import json
 
 #salva o dicionario parametro no arquivo "testando.dat" no formato json
-def salvar_dicio(dicionario):
+def salvar(dicionario):
     with open("testando.dat", "w") as f:
         f.write(json.dumps(dicionario))
 
 
 #carrega o dicionario
-def carregar_dicio():
+def carregar():
     with open("testando.dat") as f:
         return json.loads(f.read())
 

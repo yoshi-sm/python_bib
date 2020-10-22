f23d494062207160366d8e899b72846ca8826f63
João Pedro Israel <juanpeterisrael@gmail.com>
2020-10-22 20:55:28 -0300

    Checagem de livros duplicados funciona com json

--- cadastrolivro.py
+++ cadastrolivro.py
@@ -5,29 +5,36 @@ from saveload import salvar, carregar
 def cadastroLivros():
 
     #Carregando os valores no dicionario
-    # cadastro = carregar()
+    bd = carregar()
 
+    tamanho_array = len(bd)
     #Adicionando novos valores no dicionario
     titulo = input('Titulo: ')
 
-    # if titulo not in cadastro["titulo"]:
-        # cadastro["titulo"].append(titulo)
+    # Checar se o livro já existe na base de dados:
+    for i in range(tamanho_array):
+        # Percorrer toda a base de dados para encontrar um livro similar
+        if bd[i]["titulo"] == titulo:
+            print("Livro já cadastrado!")
+            # Se sim, retornar o usuário ao menu anterior
+            escolha = "z"
+            return escolha
+
     cadastro = dict(
-        titulo = titulo,
-        autores = input('Autor: '),
-        assunto = input('Categoria: '),
-        ano = input('Ano: '), 
-        localizacao = input('Localização: '),
-        data_devolucao = "",
-        alugado = "",
-        reserva = "",
-        usuario_reserva = ""
-        )
+                titulo = titulo,
+                autores = input('Autor: '),
+                assunto = input('Categoria: '),
+                ano = input('Ano: '), 
+                localizacao = input('Localização: '),
+                data_devolucao = "",
+                alugado = "",
+                reserva = "",
+                usuario_reserva = ""
+                )
 
     salvar(cadastro)
     print("Cadastro realizado!")
-    # else:
-    #     print("Titulo já cadastrado!")
+
 
     #print(cadastro)
     #Salvando os novos valores
--- saveload.py
+++ saveload.py
@@ -1,30 +1,31 @@
 import json
 import os
 
-# banco_de_dados = "bd/acervo.dat"
 banco_de_dados = "bd/acervo.json"
 
-# TODO - Precisa de trabalho aqui
+# Deletar a base de dados, iniciar array
 def reset_acervo():
     with open(banco_de_dados, "w") as f:
         f.write('[\n')
 
-        # '{"titulo": \n, "autores": \n, "assunto": \n,'
-        # '"ano": \n, "localizacao" : \n, "data_devolucao": \n,'
-        # '"alugado": \n, "reserva": \n, "usuario_reserva": \n}'
-
-#salva o dicionario parametro no arquivo "acervo.dat" no formato json
+# Salva o dicionario parametro no arquivo "acervo.json"
 def salvar(dicionario):
-    # Ler a base de dados
+
+    # Ler a base de dados, enumerar linhas
     with open(banco_de_dados, 'r') as f:
         linhas = f.readlines()
+        # # checar se o título já existe
+        # if dicionario["titulo"] in f["titulo"]:
+        #     print("Já cadastrado!")
+        #     break
 
     # Se a base não estiver vazia, ela irá terminar com ]
-    # Deletar o último ]
     with open(banco_de_dados, 'w') as f:
-            for linha in linhas:
-                if linha.strip('\n') != "]":
-                    f.write(linha)
+        # Ler todas as linhas
+        for linha in linhas:
+            # Deletar o último ]
+            if linha.strip('\n') != "]":
+                f.write(linha)
 
     # Salvar na base de dados
     with open(banco_de_dados, 'a', encoding='utf8') as f:
@@ -35,16 +36,14 @@ def salvar(dicionario):
         else:
             # Adicionar , e iniciar novo objeto
             f.write(",")
+
         # Da Append no dicionário
         f.write(json.dumps(dicionario, indent=4, ensure_ascii=False))
         # Terminar o dicionário em ]
         f.write("\n]")
 
 
-#carrega o dicionario
+# Carrega o dicionario
 def carregar():
     with open(banco_de_dados) as f:
         return json.loads(f.read())
-
-
-

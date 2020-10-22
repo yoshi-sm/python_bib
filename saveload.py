438e8b340afb7bcb8599e6968ff3be7995a9b833
João Pedro Israel <juanpeterisrael@gmail.com>
2020-10-22 12:04:07 -0300

    Tentando escrever base de dados em formato .json, problemas com []

--- saveload.py
+++ saveload.py
@@ -1,12 +1,13 @@
 import json
 
 
-# banco_de_dados = "bd/acervo.dat"
-banco_de_dados = "bd/acervo.json"
+banco_de_dados = "bd/acervo.dat"
+# banco_de_dados = "bd/acervo.json"
 
+# TODO - Precisa de trabalho aqui
 def reset_acervo():
     with open(banco_de_dados, "w") as f:
-        f.write('Adicione um livro!')
+        f.write('[]')
 
         # '{"titulo": \n, "autores": \n, "assunto": \n,'
         # '"ano": \n, "localizacao" : \n, "data_devolucao": \n,'
@@ -14,8 +15,10 @@ def reset_acervo():
 
 #salva o dicionario parametro no arquivo "acervo.dat" no formato json
 def salvar(dicionario):
-    with open(banco_de_dados, "w", encoding='utf8') as f:
-        f.write(json.dumps(dicionario, ensure_ascii=False))
+
+    with open(banco_de_dados, 'a', encoding='utf8') as f:
+        f.write(json.dumps(dicionario, indent=4, ensure_ascii=False))
+        f.write(",\n")
 
 
 #carrega o dicionario

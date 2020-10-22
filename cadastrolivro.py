0747afa57096ecd57524caeda53c71703eabb2dd
João Pedro Israel <juanpeterisrael@gmail.com>
2020-10-22 11:11:52 -0300

    Mudando codificação para .json, adicionando suporte para UTF-8

--- cadastrolivro.py
+++ cadastrolivro.py
@@ -5,33 +5,45 @@ from saveload import salvar, carregar
 def cadastroLivros():
 
     #Carregando os valores no dicionario
-    cadastro = carregar()
+    # cadastro = carregar()
 
     #Adicionando novos valores no dicionario
     titulo = input('Titulo: ')
-    if titulo not in cadastro["titulo"]:
-        cadastro["titulo"].append(titulo)
-        cadastro["autores"].append(input('Autor: '))
-        #cadastro["categorias"].append(input('Categoria: '))
-        #cadastro["tematicas"].append(input('Tematica: '))
-        cadastro["assunto"].append(input('Assunto: '))
-        cadastro["ano"].append(input('Ano: '))
-        cadastro["localizacao"].append(input('Localização: '))
-        #cadastro["unidades"].append(int(input('Unidades: ')))
-        #unidades_reserva = int(input("Unidades para reserva/aluguel: "))
-        #while unidades_reserva >= cadastro["unidades"][-1]:
-        #    print("valor para unidades para reserva/aluguel inválido, tente novamente: ")
-        #    unidades_reserva = int(input("Unidades para reserva/aluguel: "))
-        #cadastro["unidades_reserva"].append(unidades_reserva)
-        cadastro["data_devolucao"].append("")
-        cadastro["alugado"].append(False)
-        cadastro["usuario_reserva"].append("")
-        cadastro["reserva"].append(False)
 
-        salvar(cadastro)
-        print("Cadastro realizado!")
-    else:
-        print("Titulo já cadastrado!")
+    # if titulo not in cadastro["titulo"]:
+        # cadastro["titulo"].append(titulo)
+    cadastro = dict(
+        titulo = titulo,
+        autores = input('Autor: '),
+        assunto = input('Categoria: '),
+        ano = input('Ano: '), 
+        localizacao = input('Localização: '),
+        data_devolucao = "",
+        alugado = "",
+        reserva = "",
+        usuario_reserva = ""
+        )
+        # cadastro["autores"].append(input('Autor: '))
+        # #cadastro["categorias"].append(input('Categoria: '))
+        # #cadastro["tematicas"].append(input('Tematica: '))
+        # cadastro["assunto"].append(input('Assunto: '))
+        # cadastro["ano"].append(input('Ano: '))
+        # cadastro["localizacao"].append(input('Localização: '))
+        # #cadastro["unidades"].append(int(input('Unidades: ')))
+        # #unidades_reserva = int(input("Unidades para reserva/aluguel: "))
+        # #while unidades_reserva >= cadastro["unidades"][-1]:
+        # #    print("valor para unidades para reserva/aluguel inválido, tente novamente: ")
+        # #    unidades_reserva = int(input("Unidades para reserva/aluguel: "))
+        # #cadastro["unidades_reserva"].append(unidades_reserva)
+        # cadastro["data_devolucao"].append("")
+        # cadastro["alugado"].append(False)
+        # cadastro["usuario_reserva"].append("")
+        # cadastro["reserva"].append(False)
+
+    salvar(cadastro)
+    print("Cadastro realizado!")
+    # else:
+    #     print("Titulo já cadastrado!")
 
     #print(cadastro)
     #Salvando os novos valores
--- saveload.py
+++ saveload.py
@@ -1,18 +1,21 @@
 import json
 
 
-banco_de_dados = "bd/acervo.dat"
+# banco_de_dados = "bd/acervo.dat"
+banco_de_dados = "bd/acervo.json"
 
 def reset_acervo():
     with open(banco_de_dados, "w") as f:
-        f.write('{"titulo": [], "autores": [], "assunto": [],'
-                '"ano": [], "localizacao" : [], "data_devolucao": [],'
-                '"alugado": [], "reserva": [], "usuario_reserva": []}')
+        f.write('Adicione um livro!')
+
+        # '{"titulo": \n, "autores": \n, "assunto": \n,'
+        # '"ano": \n, "localizacao" : \n, "data_devolucao": \n,'
+        # '"alugado": \n, "reserva": \n, "usuario_reserva": \n}'
 
 #salva o dicionario parametro no arquivo "acervo.dat" no formato json
 def salvar(dicionario):
-    with open(banco_de_dados, "w") as f:
-        f.write(json.dumps(dicionario))
+    with open(banco_de_dados, "w", encoding='utf8') as f:
+        f.write(json.dumps(dicionario, ensure_ascii=False))
 
 
 #carrega o dicionario

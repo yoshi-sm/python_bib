d327470ba043751213538d1b9d4789e6d54e9a3d
João Pedro Israel <juanpeterisrael@gmail.com>
2020-10-22 15:11:17 -0300

    Consegui fazer a escrita do .json funcionar 😎

--- cadastrolivro.py
+++ cadastrolivro.py
@@ -23,22 +23,6 @@ def cadastroLivros():
         reserva = "",
         usuario_reserva = ""
         )
-        # cadastro["autores"].append(input('Autor: '))
-        # #cadastro["categorias"].append(input('Categoria: '))
-        # #cadastro["tematicas"].append(input('Tematica: '))
-        # cadastro["assunto"].append(input('Assunto: '))
-        # cadastro["ano"].append(input('Ano: '))
-        # cadastro["localizacao"].append(input('Localização: '))
-        # #cadastro["unidades"].append(int(input('Unidades: ')))
-        # #unidades_reserva = int(input("Unidades para reserva/aluguel: "))
-        # #while unidades_reserva >= cadastro["unidades"][-1]:
-        # #    print("valor para unidades para reserva/aluguel inválido, tente novamente: ")
-        # #    unidades_reserva = int(input("Unidades para reserva/aluguel: "))
-        # #cadastro["unidades_reserva"].append(unidades_reserva)
-        # cadastro["data_devolucao"].append("")
-        # cadastro["alugado"].append(False)
-        # cadastro["usuario_reserva"].append("")
-        # cadastro["reserva"].append(False)
 
     salvar(cadastro)
     print("Cadastro realizado!")
--- saveload.py
+++ saveload.py
@@ -1,13 +1,13 @@
 import json
+import os
 
-
-banco_de_dados = "bd/acervo.dat"
-# banco_de_dados = "bd/acervo.json"
+# banco_de_dados = "bd/acervo.dat"
+banco_de_dados = "bd/acervo.json"
 
 # TODO - Precisa de trabalho aqui
 def reset_acervo():
     with open(banco_de_dados, "w") as f:
-        f.write('[]')
+        f.write('[\n')
 
         # '{"titulo": \n, "autores": \n, "assunto": \n,'
         # '"ano": \n, "localizacao" : \n, "data_devolucao": \n,'
@@ -15,10 +15,30 @@ def reset_acervo():
 
 #salva o dicionario parametro no arquivo "acervo.dat" no formato json
 def salvar(dicionario):
+    # Ler a base de dados
+    with open(banco_de_dados, 'r') as f:
+        linhas = f.readlines()
 
+    # Se a base não estiver vazia, ela irá terminar com ]
+    # Deletar o último ]
+    with open(banco_de_dados, 'w') as f:
+            for linha in linhas:
+                if linha.strip('\n') != "]":
+                    f.write(linha)
+
+    # Salvar na base de dados
     with open(banco_de_dados, 'a', encoding='utf8') as f:
+        # Se a base de dados estiver vazio, adiconar chaves
+        if os.stat(banco_de_dados).st_size == 0:
+            f.write('[\n')
+
+        else:
+            # Adicionar , e iniciar novo objeto
+            f.write(",")
+        # Da Append no dicionário
         f.write(json.dumps(dicionario, indent=4, ensure_ascii=False))
-        f.write(",\n")
+        # Terminar o dicionário em ]
+        f.write("\n]")
 
 
 #carrega o dicionario
